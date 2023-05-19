47f7994156d62ec970c5b703110cf9690a6b773b
khosronz <khosronz.com@gmail.com>
2023-05-19 06:06:49 +0330

    Modify rtl and change logo to Moharam ol Haram

--- src/pages/UiElements/Alerts.tsx
+++ src/pages/UiElements/Alerts.tsx
@@ -9,7 +9,7 @@ const Alerts = () => {
       <div className="rounded-sm border border-stroke bg-white p-4 shadow-default dark:border-strokedark dark:bg-boxdark md:p-6 xl:p-9">
         <div className="flex flex-col gap-7.5">
           {/* <!-- Alerts Item --> */}
-          <div className="flex w-full border-l-6 border-warning bg-warning bg-opacity-[15%] px-7 py-8 shadow-md dark:bg-[#1B1B24] dark:bg-opacity-30 md:p-9">
+          <div className="flex w-full border-r-6 border-warning bg-warning bg-opacity-[15%] px-7 py-8 shadow-md dark:bg-[#1B1B24] dark:bg-opacity-30 md:p-9">
             <div className="ml-5 flex h-9 w-9 items-center justify-center rounded-lg bg-warning bg-opacity-30">
               <svg
                 width="19"
@@ -26,17 +26,15 @@ const Alerts = () => {
             </div>
             <div className="w-full">
               <h5 className="mb-3 text-lg font-semibold text-[#9D5425]">
-                Attention needed
+                نیاز مند توجه
               </h5>
               <p className="leading-relaxed text-[#D0915C]">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when
+                پیام هایی که نیازمند توجه هستند و بهتر است کاربر در آنها اقداماتی انجام دهد.
               </p>
             </div>
           </div>
           {/* <!-- Alerts Item --> */}
-          <div className="flex w-full border-l-6 border-[#34D399] bg-[#34D399] bg-opacity-[15%] px-7 py-8 shadow-md dark:bg-[#1B1B24] dark:bg-opacity-30 md:p-9">
+          <div className="flex w-full border-r-6 border-[#34D399] bg-[#34D399] bg-opacity-[15%] px-7 py-8 shadow-md dark:bg-[#1B1B24] dark:bg-opacity-30 md:p-9">
             <div className="ml-5 flex h-9 w-full max-w-[36px] items-center justify-center rounded-lg bg-[#34D399]">
               <svg
                 width="16"
@@ -57,13 +55,12 @@ const Alerts = () => {
               پیام با موفقیت ارسال شد
               </h5>
               <p className="text-base leading-relaxed text-body">
-                لورم به سادگی متن ساختگی چاپ و حروفچوب است
-                صنعت.
+              پیام هایی که نشان دهنده موفق بودن عملیات انجام شده می باشد.
               </p>
             </div>
           </div>
           {/* <!-- Alerts Item --> */}
-          <div className="flex w-full border-l-6 border-[#F87171] bg-[#F87171] bg-opacity-[15%] px-7 py-8 shadow-md dark:bg-[#1B1B24] dark:bg-opacity-30 md:p-9">
+          <div className="flex w-full border-r-6 border-[#F87171] bg-[#F87171] bg-opacity-[15%] px-7 py-8 shadow-md dark:bg-[#1B1B24] dark:bg-opacity-30 md:p-9">
             <div className="ml-5 flex h-9 w-full max-w-[36px] items-center justify-center rounded-lg bg-[#F87171]">
               <svg
                 width="13"
@@ -85,9 +82,7 @@ const Alerts = () => {
               </h5>
               <ul>
                 <li className="leading-relaxed text-[#CD5D5D]">
-                لورم به سادگی متن ساختگی چاپ و حروفچوب است
-                صنعت.
-
+                  پیام هایی که نیازمند توجه فوری می باشند.
                 </li>
               </ul>
             </div>
