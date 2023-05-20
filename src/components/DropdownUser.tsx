a6361341e9ce5de2c28946f08f6bb2fab0526809
khosronz <khosronz.com@gmail.com>
2023-05-20 10:52:42 +0330

    Modify text

--- src/components/DropdownUser.tsx
+++ src/components/DropdownUser.tsx
@@ -47,7 +47,7 @@ const DropdownUser = () => {
           <span className="block text-sm font-medium text-black dark:text-white">
           شهید همت
           </span>
-          <span className="block text-xs">طراح UX</span>
+          <span className="block text-xs">شهید دفاع مقدس</span>
         </span>
 
         <span className="h-12 w-12 rounded-full">
